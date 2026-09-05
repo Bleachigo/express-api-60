b347854c458b529ac803efef8f73c88da84675d8
Artem <artwhite2012@gmail.com>
2026-09-05 13:40:40 +0300

    Create a class AppError to extend a Error object and use it in Not Found middleware

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ import express from 'express';
 import rootRoutes from './src/routes/rootRoutes.js';
 import userRoutes from './src/routes/userRoutes.js';
 import articleRoutes from './src/routes/articleRoutes.js';
-// import morgan from 'morgan';
 import { logRequest } from './src/middleware/logger.middleware.js';
+import { notFound } from './src/middleware/not-found.middleware.js';
+import { errorHandler } from './src/middleware/error.middleware.js';
 
 export const app = express();
 
 app.use(logRequest);
-// app.use(morgan('dev'));
 app.use('/', rootRoutes);
 app.use('/', userRoutes);
 app.use('/', articleRoutes);
+app.use(notFound);
+app.use(errorHandler);
