a3f4ec810b444bea455934494d9008e62649262c
Artem <artwhite2012@gmail.com>
2026-09-05 11:52:38 +0300

    Remove morgan logger and decide to write my own

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import express from 'express';
 import rootRoutes from './src/routes/rootRoutes.js';
 import userRoutes from './src/routes/userRoutes.js';
 import articleRoutes from './src/routes/articleRoutes.js';
-import morgan from 'morgan';
+// import morgan from 'morgan';
+import { logRequest } from './src/middleware/logger.middleware.js';
 
 export const app = express();
 
-app.use(morgan('dev'));
+app.use(logRequest);
+// app.use(morgan('dev'));
 app.use('/', rootRoutes);
 app.use('/', userRoutes);
 app.use('/', articleRoutes);
--- src/middleware/logger.middleware.js
+++ src/middleware/logger.middleware.js
@@ -1,6 +1,6 @@
 export const logRequest = (req, res, next) => {
   console.log(
-    `${new Date().toLocaleString()} - ${req.method} request to ${req.originalUrl}`,
+    `${new Date().toLocaleString()} - ${req.method} request to ${req.originalUrl} - ${res.statusCode}`,
   );
 
   next();
