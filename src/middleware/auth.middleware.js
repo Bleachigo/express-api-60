e5277d0819933fc8abf3a6c1132c8c8ba32aa075
Artem <artwhite2012@gmail.com>
2026-09-07 16:48:00 +0300

    Improve middleware logic for learning purpose
    Hardcode 2 useres:
    Username: "admin"
    Password: "admin123"
    -------------------
    Username: "user"
    Password: "user123"

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,3 +1,4 @@
+import { Buffer } from 'node:buffer';
 import { AppError } from '../utils/AppError.js';
 
 export const basicAuth = (req, res, next) => {
@@ -7,5 +8,33 @@ export const basicAuth = (req, res, next) => {
     return next(new AppError('Authorization header is required', 401));
   }
 
-  next();
+  if (!authHeader.startsWith('Basic ')) {
+    return next(new AppError('Basic authorization is required', 401));
+  }
+
+  const encodedCredentials = authHeader.split(' ')[1];
+
+  const credentials = Buffer.from(encodedCredentials, 'base64').toString(
+    'utf8',
+  );
+
+  const [username, password] = credentials.split(':');
+
+  if (username === 'admin' && password === 'admin123') {
+    req.user = {
+      username,
+      role: 'admin',
+    };
+    return next();
+  }
+
+  if (username === 'user' && password === 'user123') {
+    req.user = {
+      username,
+      role: 'user',
+    };
+    return next();
+  }
+
+  next(new AppError('Invalid credentials', 401));
 };
