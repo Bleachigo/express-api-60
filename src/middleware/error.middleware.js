3d11389737c647ac478f51417ac6f36da4b8d664
Artem <artwhite2012@gmail.com>
2026-09-08 17:06:57 +0300

    Fix masking trusted message. Now sends own message and all 5xx go to "Internal...

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -1,12 +1,15 @@
+import { AppError } from '../utils/AppError.js';
+
 export const errorHandler = (err, req, res, next) => {
   if (res.headersSent) return next(err);
 
   const statusCode = err.statusCode || err.status || 500;
+  const isTrustedError = err instanceof AppError;
 
   if (statusCode >= 500) console.error(err);
 
   res
     .status(statusCode)
     .type('text/plain')
-    .send(statusCode >= 500 ? 'Internal server error' : err.message);
+    .send(isTrustedError ? err.message : 'Internal server error');
 };
